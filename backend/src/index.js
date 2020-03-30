111e0c6192f062f5e144ce6ab23dc3cf55ddc2c8
Mayara Barbosa <mayara.barbosa2@hotmail.com>
2020-03-29 21:59:48 -0300

    add validators

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const {errors} = require('celebrate');
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
